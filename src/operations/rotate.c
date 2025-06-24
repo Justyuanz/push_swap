cdd0a802159e4c1b6ba9589adb0d0e77821ec559
Jingyuan Zhang <justyuan.z@gmail.com>
2025-06-24 18:34:02 +0300

    vec

--- src/operations/rotate.c
+++ src/operations/rotate.c
@@ -1,3 +1,17 @@
 #include "push_swap.h"
 
-void	ft_ra()
+void	ft_ra(t_vec *stack_a)
+{
+	int tmp;
+	int	i;
+
+	i = 1;
+	if (!stack_a || !stack_a->memory)
+		return;
+	tmp = stack_a->memory[0];
+	while(i < stack_a->len)
+	{
+		stack_a->memory
+		i++;
+	}
+}
